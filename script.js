45be3edd8a49ea581b8f63e8dfdb27260ac8cecf
MeinInfoKurs <meininfokurs@gmail.com>
2025-02-26 06:37:34 +0100

    Created
    Created JS Script to work with Website

---
+++ script.js
@@ -0,0 +1,15 @@
+        function toggleMenu() {
+            document.getElementById("sidebar").classList.toggle("active");
+        }
+        document.addEventListener("click", function(event) {
+    let menu = document.getElementById("sidebar");
+    let hamburger = document.querySelector(".hamburger");
+    
+    if (!menu.contains(event.target) && !hamburger.contains(event.target)) {
+        menu.classList.remove("active");
+    }
+});
+
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+    });
